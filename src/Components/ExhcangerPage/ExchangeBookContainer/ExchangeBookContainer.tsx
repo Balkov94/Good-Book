2a4a7bf93206a645cd9de2378ed3a21ddd3aa120
N.Balkov <balkov_94@abv.bg>
2022-09-15 22:53:09 +0300

    exchange page added few styles

---
+++ src/Components/ExhcangerPage/ExchangeBookContainer/ExchangeBookContainer.tsx
@@ -0,0 +1,11 @@
+import styles from "ExBookC.module.css";
+
+function ExchangeBookContainer() {
+   return (  
+      <div>
+
+      </div>
+   );
+}
+
+export default ExchangeBookContainer;
--- src/Components/ExhcangerPage/ExchangerPage.tsx
+++ src/Components/ExhcangerPage/ExchangerPage.tsx
@@ -1,9 +1,20 @@
+import ExchangeBookContainer from './ExchangeBookContainer/ExchangeBookContainer';
+import styles from './ExchangerPage.module.css';
+
 function ExchangerPage() {
-   return ( 
-      <div>
-         
+   return (
+      <div className={styles.xPageMainContainer} >
+         <div className={styles.topImgContainer}></div>
+         <h6>Here you can find your new &#128214;. Just have to
+            <span className={styles.clickIconSpan}> 
+            <img src={require("./clickIcon.png")} alt="click icon" style={{width:"30px",marginRight:"8px"}}/>
+            </span>
+            and contact the owner &#129309;	
+         </h6>
+
+         <ExchangeBookContainer/>
       </div>
-    );
+   );
 }
 
 export default ExchangerPage;
